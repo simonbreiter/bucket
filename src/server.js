4e7c5fa0da1f32770631fae9a36b869bdf67202c
Simon Breiter <hello@simonbreiter.com>
2019-05-08 13:16:09 +0200

    feat: use object notation for schema

--- src/server.js
+++ src/server.js
@@ -1,6 +1,40 @@
 const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const {
+  buildSchema,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLString
+} = require('graphql')
+
 const port = process.env.PORT || 3000
 const app = express()
+
+// Resolver
+const hello = () => 'Hello world!'
+
+// Schema
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    description: 'Possible queries',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve: hello
+      }
+    }
+  })
+})
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    graphiql: !process.env.TESTING
+  })
+)
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!')
--- src/server.test.js
+++ src/server.test.js
@@ -1,11 +1,23 @@
 const request = require('supertest')
 const express = require('express')
+const axios = require('axios')
 const app = require('./server')
 
 test('server is online', () => {
-  return request(app)
+  request(app)
     .get('/')
     .then(response => {
       expect(response.statusCode).toBe(200)
+    })
+})
+
+test('hello world from graphql', async done => {
+  const expected = { data: { hello: 'Hello world!' } }
+  request(app)
+    .post('/graphql')
+    .send({ query: '{hello}' })
+    .then(response => {
+      expect(response.body).toEqual(expected)
+      done()
     })
 })
